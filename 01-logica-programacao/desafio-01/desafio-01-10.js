863b7a0d6b21d59ae6678d406f4687dde39b8457
Romulo Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-01-18 21:25:21 -0300

    desafio-01-10

---
+++ 01-logica-programacao/desafio-01/desafio-01-10.js
@@ -0,0 +1,13 @@
+/*
+Peça ao usuário para digitar sua idade usando um prompt
+e armazene-a na variável idade.
+
+Agora, caso a idade seja maior ou igual que 18,
+exiba um alerta com a mensagem "Pode tirar a habilitação!".
+*/
+
+const idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+  alert("Pode tirar a habilitação!");
+}
