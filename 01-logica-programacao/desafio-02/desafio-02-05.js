62741bb54dff16d3af8f62a8dec441559a1c0de9
Romulo Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-01-18 21:26:40 -0300

    desafio-02-05

---
+++ 01-logica-programacao/desafio-02/desafio-02-05.js
@@ -0,0 +1,8 @@
+/*
+Peça ao usuário para inserir seu nome usando prompt.
+Em seguida, mostre um alerta de boas-vindas usando esse nome.
+*/
+
+const inNome = prompt("Digite seu nome: ");
+
+alert(`Seja bem-vindo ${inNome}!`);
